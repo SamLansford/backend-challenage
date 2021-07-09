bcc987540e614bf007359d29f4ca23c44180491e
Sam Lansford <sam.j.lansford@gmail.com>
2021-07-09 11:27:21 -0500

    first commit

---
+++ src/test/java/com/example/backendchallenage/BackendChallenageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendchallenage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallenageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
