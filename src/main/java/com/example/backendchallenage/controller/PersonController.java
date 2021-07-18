21ffb80f9629507de606855c84a6396e73f21c22
Sam Lansford <sam.j.lansford@gmail.com>
2021-07-18 17:25:25 -0500

    messing with security stuff

--- src/main/java/com/example/backendchallenage/controller/PersonController.java
+++ src/main/java/com/example/backendchallenage/controller/PersonController.java
@@ -1,9 +1,14 @@
 package com.example.backendchallenage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PersonController {
 
+    @GetMapping("/")
+    public String showHomePage() {
+        return "person.index";
+    }
 
 }
