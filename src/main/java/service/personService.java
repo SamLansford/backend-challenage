1e4155addd087eca2ff0c63c2eb3c49491de1310
Sam Lansford <sam.j.lansford@gmail.com>
2021-07-29 15:23:11 -0500

    setting up service

---
+++ src/main/java/service/personService.java
@@ -0,0 +1,18 @@
+package service;
+
+import com.example.backendchallenage.models.Person;
+import com.example.backendchallenage.repositories.PersonRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class personService {
+
+    @Autowired private PersonRepo repo;
+
+    public List<Person> listAll() {
+        return repo.findAll();
+    }
+}
