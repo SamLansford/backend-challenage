c8e2c9edbaf84075d84663193564728a7c7a869c
Sam Lansford <sam.j.lansford@gmail.com>
2021-07-30 15:56:11 -0500

    making changes in the personController

--- src/main/java/com/example/backendchallenage/controller/PersonController.java
+++ src/main/java/com/example/backendchallenage/controller/PersonController.java
@@ -2,23 +2,23 @@ package com.example.backendchallenage.controller;
 
 import com.example.backendchallenage.models.Person;
 import com.example.backendchallenage.repositories.PersonRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import service.personService;
 
 @Controller
 public class PersonController {
 
+    @Autowired private personService service;
+
     private PersonRepo personDao;
 
-    @GetMapping("/")
-    public String showHomePage() {
+    @GetMapping("/people")
+    public String showHomePage(Model model) {
         return "person.index";
     }
-
-//    @GetMapping("/person")
-//    public String addPerson() {
-//
-//    }
 
 
 }
