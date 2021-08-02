80a6098fc648750d51455daae23c78730ca6083d
Sam Lansford <sam.j.lansford@gmail.com>
2021-08-02 15:48:56 -0500

    making html for people page

--- src/main/java/com/example/backendchallenage/controller/PersonController.java
+++ src/main/java/com/example/backendchallenage/controller/PersonController.java
@@ -1,7 +1,6 @@
 package com.example.backendchallenage.controller;
 
 import com.example.backendchallenage.models.Person;
-import com.example.backendchallenage.repositories.PersonRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,7 +18,7 @@ public class PersonController {
     public String showHomePage(Model model) {
         List<Person> people = service.listAll();
         model.addAttribute("people", people);
-        return "person.index";
+        return "index";
     }
 
 
